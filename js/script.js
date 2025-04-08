725824af6d8ef22549ae9a325ade6d23d23cc091
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-04-08 01:33:29 +0000

    AreaTrapezoid in HTML

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kukwac
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area of the trapezoid.
+ */
+function calculate() {
+  // input
+    let base1 = parseFloat(document.getElementById('base1').value);
+    let base2 = parseFloat(document.getElementById('base2').value);
+    let height = parseFloat(document.getElementById('height').value);
+
+  // process
+    let area = (base1 + base2) /2 * height;
+
+  // output
+    document.getElementById('area').innerHTML = 'Area is: ' + area.toFixed(1) + ' cm²'
+}
